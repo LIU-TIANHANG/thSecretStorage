e107553df791bcae34caf1e62c8a6b348764acde
LiuTianhang <E0175471@u.nus.edu>
2018-12-12 21:41:00 +0800

    Update javascipt.js

--- javascipt.js
+++ javascipt.js
@@ -1,1 +1,5 @@
 DOM: The document object model is a way to describe relationship between elements on a page, and gives us a way to traverse them
+
+eg: document.getElementsByTagName("header").getElementsByTagName("h1);
+                                                                 
+                                                                 
